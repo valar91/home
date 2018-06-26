8ffdf9168ff072e1dede072740e61e778a327e51
Li <licy02@mail.jj.cn>
2018-06-26 17:28:49 +0800

    vim

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ ln -s ./home/_bashrc ~/.bashrc
 ln -s ./home/_vimrc ~/.vimrc
 ln -s ./home/gitconfig/_gitconfig-jj ~/.gitconfig
 mkdir ~/.ssh
-cp ./home/_ssh/* ~/.ssh/
+cp ~/home/_ssh/* ~/.ssh/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
